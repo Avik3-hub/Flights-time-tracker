3c51afcf9f8f3e456b8e2dddaf9c1214e9575fed
Avik3-hub <sergey.nagibin55@gmail.com>
2026-08-23 01:47:59 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,7 +46,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
-implementation("org.apache.poi:poi-oxml:5.2.5")
+implementation("org.dhatim:fastexcel:0.15.3")
     // Jetpack Compose (BOM)
     val composeBom = platform("androidx.compose:compose-bom:2024.02.00")
     implementation(composeBom)
