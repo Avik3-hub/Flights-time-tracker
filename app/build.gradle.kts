b4c35f918398e7142fa8182e6a14d35e429b24c8
Avik3-hub <sergey.nagibin55@gmail.com>
2026-08-23 14:15:25 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.8.2")
 implementation("org.dhatim:fastexcel:0.15.3")
 implementation("org.dhatim:fastexcel-reader:0.15.3")
+implementation("androidx.compose.material:material-icons-extended")
     // Jetpack Compose (BOM)
     val composeBom = platform("androidx.compose:compose-bom:2024.02.00")
     implementation(composeBom)
