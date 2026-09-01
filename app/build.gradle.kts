fe0f3b808654ada4e8af16e758fc1c7c9880cb70
Avik3-hub <sergey.nagibin55@gmail.com>
2026-09-01 20:29:37 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.flightlog"
         minSdk = 26
         targetSdk = 34
-        versionCode = 84
-        versionName = "1.0.94"
+        versionCode = 10
+        versionName = "1.0.11"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
