f64aa70168ceccde42fb298bfbf31feae8e9ce71
Avik3-hub <sergey.nagibin55@gmail.com>
2026-09-01 12:36:35 +0300

    Update AppDatabase.kt

--- app/src/main/java/com/example/flightlog/data/db/AppDatabase.kt
+++ app/src/main/java/com/example/flightlog/data/db/AppDatabase.kt
@@ -11,7 +11,7 @@ import androidx.room.RoomDatabase
         DutyEntity::class,
         TariffEntity::class
     ],
-    version = 3,
+    version = 4,
     exportSchema = false
 )
 abstract class AppDatabase : RoomDatabase() {
