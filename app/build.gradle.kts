85d1f56fd13a6d27e112c5bc91131fe10afbfb03
Avik3-hub <sergey.nagibin55@gmail.com>
2026-09-05 15:20:19 +0300

    Increment versionCode from 19 to 20

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.example.flightlog"
         minSdk = 26
         targetSdk = 34
-        versionCode = 19
+        versionCode = 20
         versionName = "1.1.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
